9e4f38e978182f9b841dd3bfce5d391fdf60be44
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-01-10 17:15:07 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,65 @@
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
+// import cookieSession from 'cookie-session';   // import cookieSession not work for nestjs
+const cookieSession = require('cookie-session');
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    type: 'postgres',
+                    host: 'localhost',
+                    port: 5432,
+                    username: config.get<string>('USERNAME'),
+                    password: config.get<string>('PASSWORD'),
+                    database: config.get<string>('DB_NAME'),
+                    entities: [User, Report],
+                    synchronize: true,
+                };
+            },
+        }),
+        // TypeOrmModule.forRoot({
+        //     type: 'postgres',
+        //     host: 'localhost',
+        //     port: 5432,
+        //     username: 'postgres',
+        //     password: 'postgres',
+        //     database: 'carPricingNestJS',
+        //     entities: [User, Report],
+        //     synchronize: true, // only for dev environment, not production
+        // }),
+        UsersModule,
+        ReportsModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_PIPE,
+            useValue: new ValidationPipe({
+                whitelist: true,
+            }),
+        },
+    ],
+})
+export class AppModule {
+    constructor(private configService: ConfigService) {}
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(cookieSession({ keys: [this.configService.get('COOKIE_KEY')] })).forRoutes('*');
+    }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,96 @@
+import {
+    Body,
+    Controller,
+    Post,
+    Get,
+    Patch,
+    Delete,
+    Param,
+    Query,
+    NotFoundException,
+    // UseInterceptors,
+    // ClassSerializerInterceptor,
+    Session,
+    UseGuards
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateuserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorators';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
+
+
+@Controller('auth')
+@Serialize(UserDto) // no password return for user
+
+
+export class UsersController {
+    constructor(
+        private userService: UsersService,
+        private authService: AuthService
+        ) {}
+
+    // @Get('/whoami')
+    // whoAmI(@Session() session: any) {
+    //     return this.userService.findOne(session.userId);
+    // }
+
+    // Custom Decorator Current User
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user: User) {
+        return user;
+    }
+
+    @Post('/signup')
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        // this.userService.create(body.email, body.password);  // creating User with no authentication
+        const user = await this.authService.signup(body.email, body.password);  // creating User with authentication
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);  // login User with authentication
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signout(@Session() session: any) {
+        session.userId = null;
+    }
+
+    // @UseInterceptors(new SerializerInterceptor(UserDto)) // no password return with Get method
+    // @Serialize(UserDto)
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+        // console.log('handler is running');
+        const user = await this.userService.findOne(parseInt(id));
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+    // @Serialize(UserDto)     // no password return for user
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateuserDto) {
+        return this.userService.update(parseInt(id), body);
+    }
+}
